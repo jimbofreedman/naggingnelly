98c9426316927dc334fd444465926e6115f9857b
Jimbo Freedman <jimbo@freedman.io>
2024-04-28 12:39:03 +0200

    feat(todo): Import Flutter Bloc Todo tutorial

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,51 +1,20 @@
 import 'dart:async';
 import 'dart:developer';
-import 'dart:ui';
 
 import 'package:bloc/bloc.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/widgets.dart';
+import 'package:naggingnelly/app/app.dart';
+import 'package:naggingnelly/app/app_bloc_observer.dart';
+import 'package:todos_api/todos_api.dart';
+import 'package:todos_repository/todos_repository.dart';
 
-import 'package:naggingnelly/firebase_options_development.dart';
-
-class AppBlocObserver extends BlocObserver {
-  const AppBlocObserver();
-
-  @override
-  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
-    super.onChange(bloc, change);
-    log('onChange(${bloc.runtimeType}, $change)');
-  }
-
-  @override
-  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
-    log('onError(${bloc.runtimeType}, $error, $stackTrace)');
-    super.onError(bloc, error, stackTrace);
-  }
-}
-
-Future<void> bootstrap(
-  String firebaseProject,
-  FutureOr<Widget> Function() builder,
-) async {
+Future<void> bootstrap({required TodosApi todosApi}) async {
   Bloc.observer = const AppBlocObserver();
 
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    name: firebaseProject,
-    options: DefaultFirebaseOptions.currentPlatform,
+  final todosRepository = TodosRepository(todosApi: todosApi);
+
+  runZonedGuarded(
+    () => runApp(App(todosRepository: todosRepository)),
+    (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
   );
-
-  FlutterError.onError = (errorDetails) {
-    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
-  };
-  // Pass all uncaught asynchronous errors that aren't handled by the Flutter
-  // framework to Crashlytics
-  PlatformDispatcher.instance.onError = (error, stack) {
-    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-    return true;
-  };
-
-  runApp(await builder());
 }
--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,12 @@
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:global_configuration/global_configuration.dart';
-import 'package:naggingnelly/app/app.dart';
+import 'package:local_storage_todos_api/local_storage_todos_api.dart';
 import 'package:naggingnelly/bootstrap.dart';
+import 'package:naggingnelly/firebase_options_development.dart';
 
 final Map<String, String> appSettings = {
   'googleClientId': '114263892494-jje4rupuh2qn2purcnl5255brr9b6lcr.apps.'
@@ -8,6 +14,26 @@ final Map<String, String> appSettings = {
 };
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'naggingnelly-dev',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter
+  // framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  final todosApi = LocalStorageTodosApi(
+    plugin: await SharedPreferences.getInstance(),
+  );
+
   GlobalConfiguration().loadFromMap(appSettings);
-  return bootstrap('naggingnelly-dev', () => const App());
+  return bootstrap(todosApi: todosApi);
 }
